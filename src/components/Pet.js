a4992deee4c5fa4f23e7fe32d8fd56f96d82c141
vissankar <vissankar@ebay.com>
2020-06-04 02:02:43 -0700

    config setup completed

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,8 +1,11 @@
 import React from "react";
+
 export default function Pet({ name, type, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { key: 1 }, name),
-    React.createElement("h2", { key: 2 }, type),
-    React.createElement("h2", { key: 3 }, breed),
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{type}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
