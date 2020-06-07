3d953edd295f8892c2f4c66a3b86edb56b90f3cb
Vishnu Sankar <vishgitt@gmail.com>
2020-06-07 12:59:08 -0700

    add reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./components/SearchParams";
-
+import Details from "./components/Details";
+import {Router,Link} from "@reach/router"
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <header><Link to="/">Adopt Me!</Link></header>
+      <Router>
+        <SearchParams path="/"/>
+        <Details path="/details/:id"/>
+      </Router>
     </div>
   );
 };
